d7a0d8de598215cf4aea58e36f9de602adb8173b
thesivis <raphael@ic.ufmt.br>
2023-02-23 22:26:08 -0400

    Commit inicial

---
+++ mibanca/src/main/java/br/ufmt/mibanca/MibancaApplication.java
@@ -0,0 +1,13 @@
+package br.ufmt.mibanca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MibancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MibancaApplication.class, args);
+	}
+
+}
---
+++ mibanca/src/test/java/br/ufmt/mibanca/MibancaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufmt.mibanca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MibancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
