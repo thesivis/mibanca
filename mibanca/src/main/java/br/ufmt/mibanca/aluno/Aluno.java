d0917c3cb4352524e554b1a060fbd242b47516b4
isconaifa <=>
2023-03-02 20:56:15 -0400

    cria a entidade Aluno
    Este commit cria a entidade aluno utilizando o JPA que é utilizado no projeto

---
+++ mibanca/src/main/java/br/ufmt/mibanca/aluno/Aluno.java
@@ -0,0 +1,58 @@
+package br.ufmt.mibanca.aluno;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "aluno")
+@SequenceGenerator(name = "seqAluno", sequenceName =  "seq_aluno_id", allocationSize = 1)
+@Getter
+@Setter
+public class Aluno {
+
+    @Id
+    @GeneratedValue(generator =  "seqAluno", strategy = GenerationType.SEQUENCE)
+    private int id;
+    @Column(name = "matricula", length = 100)
+    private String matricula;
+    @Column(name = "endereco")
+    
+    private String endereco;
+    @Column(name =  "endereco", length =  100)
+    private String email;
+    @Column(name =  "email", length =  100)
+    //private Pesssoa pessoa;
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Aluno other = (Aluno) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+}
+
