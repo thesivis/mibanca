8c1cae69953c9cc34e0e07361a0346d4b3530f6d
marcosARGomes <marcosandrabahia@gmail.com>
2023-03-09 19:31:03 -0400

    Realizado remoção dos espaços, alteração do nome do generetor e padronização dos nomes das colunas

--- mibanca/src/main/java/br/ufmt/mibanca/formacao/Formacao.java
+++ mibanca/src/main/java/br/ufmt/mibanca/formacao/Formacao.java
@@ -7,35 +7,26 @@ import javax.persistence.GenerationType;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-
 import org.springframework.data.annotation.Id;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-
 @Entity
 @Table(name = "formacao")
 @SequenceGenerator(name="seqFormacao", sequenceName = "seq_formacao_id", allocationSize = 1)
 @Getter
 @Setter
-
-
 public class Formacao {
     @Id
-    @GeneratedValue (generator = "seq_formacao_id", strategy = GenerationType.SEQUENCE)
+    @GeneratedValue (generator = "seqFormacao", strategy = GenerationType.SEQUENCE)
     private int id;
-
     @Column(name = "nome", length = 200)
     private String nome;
-
-    @Column(name="cargaHoraria")
+    @Column(name="carga_horaria")
     private float cargaHoraria;
-
     @ManyToOne 
     //private Curso curso;
     //private Tipo_Banca tipo_Banca;
-
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -43,7 +34,6 @@ public class Formacao {
         result = prime * result + id;
         return result;
     }
-
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
