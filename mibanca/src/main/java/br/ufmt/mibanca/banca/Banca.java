01db30383e3d83d7d080c97bbd610721961e59dc
RodrigoCelso <rodrigo.ca.bonfim@gmail.com>
2023-03-09 22:04:13 -0400

    Mapeou as relações da entidade Banca
    Criei os relacionamentos ManyToOne e OneToMany

--- mibanca/src/main/java/br/ufmt/mibanca/banca/Banca.java
+++ mibanca/src/main/java/br/ufmt/mibanca/banca/Banca.java
@@ -5,21 +5,31 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import java.util.Date;
+import java.util.List;
 
 import org.hibernate.annotations.ManyToAny;
 
+import br.ufmt.mibanca.apresentacao.Apresentacao;
+import br.ufmt.mibanca.ata.Ata;
+import br.ufmt.mibanca.avaliacao.Avaliacao;
+import br.ufmt.mibanca.cidade.Cidade;
+import br.ufmt.mibanca.formacao.Formacao;
+import br.ufmt.mibanca.participacao.Participacao;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity // informando que se trata de uma entidade
-@Table(name = "Banca") // inserindo nome da tabela
+@Table(name = "banca") // inserindo nome da tabela
 @SequenceGenerator(name = "seqBanca", sequenceName = "seq_banca_id", allocationSize = 1)
 @Getter
 @Setter
@@ -32,29 +42,43 @@ import lombok.Setter;
 
 public class Banca {
     @Id
-    @GeneratedValue(generator = "seq_banca_id", strategy = GenerationType.SEQUENCE) // definindo atributo como atributo gerado.
-    private int id;
+    @GeneratedValue(generator = "seqBanca", strategy = GenerationType.SEQUENCE) // definindo atributo como atributo gerado.
+    private int id_banca;
     // generator = quem
     // strategy = tipo do gerador, que será gerencial (postgresql)
     //
 
-    //private int id_cidade;
-    //private int id_formacao;
-
-     @Column(name = "Banca_DataBanca")
+    @Column(name = "data_banca") //caso a coluna seja String, definir length
     @Temporal(TemporalType.DATE)
-    public Date data;
+    private Date dataBanca;
 
+//---------------------------------------------
+    @ManyToOne
+    @JoinColumn(name="formacao_id")
+    private Formacao formacao;
 
-    //@ManyToOne
-    //private Formacao formacao
-    //private Cidade cidade
+    @ManyToOne
+    @JoinColumn(name="cidade_id")
+    private Cidade cidade;
+//----------------------------------------------
+    @OneToMany(mappedBy = "banca") //ata
+    private List<Ata> atas;
+
+    @OneToMany(mappedBy = "banca") //apresentação
+    private List<Apresentacao> apresentacoes;
+
+    @OneToMany(mappedBy = "banca") //avaliação
+    private List<Avaliacao> avaliacoes;
+
+    @OneToMany(mappedBy = "banca") //participação
+    private List<Participacao> participacoes;
+//---------------------------------------------
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + id;
+        result = prime * result + id_banca;
         return result;
     }
     @Override
@@ -66,7 +90,7 @@ public class Banca {
         if (getClass() != obj.getClass())
             return false;
         Banca other = (Banca) obj;
-        if (id != other.id)
+        if (id_banca != other.id_banca)
             return false;
         return true;
     }
