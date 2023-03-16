658ba2db9582ac18bad4d38b8e7b349193496e8b
thesivis <raphael@ic.ufmt.br>
2023-03-16 19:24:28 -0400

    Arruma os imports

--- mibanca/src/main/java/br/ufmt/mibanca/pessoa/Pessoa.java
+++ mibanca/src/main/java/br/ufmt/mibanca/pessoa/Pessoa.java
@@ -1,18 +1,21 @@
 package br.ufmt.mibanca.pessoa;
 
-import java.sql.Date;
+import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import lombok.EqualsAndHashCode;
+import br.ufmt.mibanca.aluno.Aluno;
+import br.ufmt.mibanca.membro.Membro;
 import lombok.Getter;
 import lombok.Setter;
 
