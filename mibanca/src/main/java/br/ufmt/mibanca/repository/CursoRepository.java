c3a45884fc24c4040919d9efe66447460f82e32b
RicGen10 <eric.borges@sou.ufmt.br>
2023-03-30 19:51:19 -0400

    Ajustando tipo da herança do repositorio
    Ajustando de JpaRepository para JpaRepositoryImplementation.

--- mibanca/src/main/java/br/ufmt/mibanca/repository/CursoRepository.java
+++ mibanca/src/main/java/br/ufmt/mibanca/repository/CursoRepository.java
@@ -1,9 +1,9 @@
 package br.ufmt.mibanca.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 
 import br.ufmt.mibanca.entity.Curso;
 
-public interface CursoRepository extends JpaRepository<Curso,Long> {
+public interface CursoRepository extends JpaRepositoryImplementation<Curso,Long> {
     
 }
