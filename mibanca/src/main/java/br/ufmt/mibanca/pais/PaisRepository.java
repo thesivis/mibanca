034683e0521e50c5e86b57335140816f3febad2d
jogodoy8 <joelmagodoy8@gmail.com>
2023-03-30 21:06:07 -0400

    Remove pais repositório

--- mibanca/src/main/java/br/ufmt/mibanca/pais/PaisRepository.java
+++
@@ -1,8 +0,0 @@
-package br.ufmt.mibanca.pais;
-
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
-public interface PaisRepository 
-    extends JpaRepositoryImplementation<Pais, Integer>{
-
-    }
