26e6b594054c50ce57cf02d06f4a48876d2c67ad
jogodoy8 <joelmagodoy8@gmail.com>
2023-03-30 20:44:19 -0400

    Criar o repostório da entidade Estado

---
+++ mibanca/src/main/java/br/ufmt/mibanca/estado/EstadoRepository.java
@@ -0,0 +1,8 @@
+package br.ufmt.mibanca.estado;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+public interface EstadoRepository 
+    extends JpaRepositoryImplementation<Estado, Integer>{
+        
+    }
