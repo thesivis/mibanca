2ed1c970258af3c64f330d66d22f9b3445ea5061
isconaifa <=>
2023-03-30 20:09:14 -0400

    Cria o repositorio da entidade Instituto
    Corrige o import da entidade Instituto.

---
+++ mibanca/src/main/java/br/ufmt/mibanca/instituto/InstitutoRepository.java
@@ -0,0 +1,7 @@
+package br.ufmt.mibanca.instituto;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+public interface InstitutoRepository extends JpaRepositoryImplementation {
+    
+}
