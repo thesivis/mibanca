f25cad697da171a01e19aceb28b5eac826a2e4dc
jogodoy8 <joelmagodoy8@gmail.com>
2023-03-30 20:17:10 -0400

    Cria o repositorio da entidade Pais

---
+++ mibanca/src/main/java/br/ufmt/mibanca/pais/PaisRepository.java
@@ -0,0 +1,8 @@
+package br.ufmt.mibanca.pais;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+public interface PaisRepository 
+    extends JpaRepositoryImplementation<Pais, Integer>{
+
+    }
