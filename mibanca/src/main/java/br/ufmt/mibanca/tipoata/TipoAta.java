709d31aae100eda6dd7c9e0616bdfeb53a46204f
thesivis <raphael@ic.ufmt.br>
2023-03-30 20:17:50 -0400

    Cria o repositório da entidade TipoAta
    Corrigi a nomeação da entidade TipoAta
    Corrigi o mapeamento da relação com a entidade Ata

--- mibanca/src/main/java/br/ufmt/mibanca/tipoata/TipoAta.java
+++ mibanca/src/main/java/br/ufmt/mibanca/tipoata/TipoAta.java
@@ -1,31 +1,39 @@
 package br.ufmt.mibanca.tipoata;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import br.ufmt.mibanca.ata.Ata;
+
 //import org.hibernate.annotations.ManyToAny;
 
-import lombok.Getter; 
+import lombok.Getter;
 import lombok.Setter; 
 
 @Entity
 @Table(name="tipoAta")
-@SequenceGenerator(name = "seqtipoAta", sequenceName = "seq_tipo_ata_id", allocationSize = 1)
+@SequenceGenerator(name = "seqTipoAta", sequenceName = "seq_tipo_ata_id", allocationSize = 1)
 @Getter
 @Setter
-public class tipoata {
+public class TipoAta {
     @Id
-    @GeneratedValue(generator = "seqtipoAta", strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(generator = "seqTipoAta", strategy = GenerationType.SEQUENCE)
     private int id;
 
     @Column(name = "descricao", length = 200)
     private String descricao;  
-    //@ManyToAny
+    
+    @OneToMany(mappedBy = "tipo")
+    private List<Ata> atas;
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -41,7 +49,7 @@ public class tipoata {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        tipoata other = (tipoata) obj;
+        TipoAta other = (TipoAta) obj;
         if (id != other.id)
             return false;
         return true;
