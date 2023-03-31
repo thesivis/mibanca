24437de1b1e44e37e7ae151dd14b8fe94ea1c0cc
= <=>
2023-03-30 20:07:36 -0400

    r-ramos97 comitando repositorio com interface

---
+++ mibanca/src/main/java/br/ufmt/mibanca/tipobanca/TipoBancaRepository.java
@@ -0,0 +1,9 @@
+package br.ufmt.mibanca.tipobanca;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+
+public interface TipoBancaRepository  extends JpaRepositoryImplementation <TipoBanca, Integer >{
+
+        
+}
