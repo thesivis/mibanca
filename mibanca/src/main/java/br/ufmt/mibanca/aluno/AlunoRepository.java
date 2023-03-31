b034a178a800bf6bcb046463c5f7d429c9df39f1
isconaifa <iscobasdostdbv@gmail.com>
2023-03-30 20:17:40 -0400

    Cria o repositório da entidade Aluno

---
+++ mibanca/src/main/java/br/ufmt/mibanca/aluno/AlunoRepository.java
@@ -0,0 +1,8 @@
+package br.ufmt.mibanca.aluno;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+public interface AlunoRepository extends JpaRepositoryImplementation <Aluno, Integer>{
+
+}
+    
