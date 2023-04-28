ef3d50bf2831c8c99d1d049ca4f559093f090180
= <=>
2023-04-27 21:09:45 -0400

    Adiciona todos os endopoints da entidade qualificaçao GET retorna todos os elementos do banco GET by id retorna um elemento especifico DELETE remove um elemento pelo id POST cadastra um elemento novo PATCH atualiza um elemento pelo id Criou a classe request e a classe response da entidade

--- mibanca/src/main/java/br/ufmt/mibanca/qualificacao/QualificacaoController.java
+++ mibanca/src/main/java/br/ufmt/mibanca/qualificacao/QualificacaoController.java
@@ -5,12 +5,16 @@ import java.util.Optional;
 
 import javax.transaction.Transactional;
 
+import org.junit.runner.Request;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.repository.query.parser.Part.IgnoreCaseType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,8 +51,9 @@ public class QualificacaoController {
             return ResponseEntity.ok().build();
 
         } catch (EmptyResultDataAccessException erro) {
-            return ResponseEntity.notFound();
+            return ResponseEntity.notFound().build();
         }
+    }
 
     @PostMapping(path = "/{id}")
     public ResponseEntity<QualificacaoResponse> RequestedById(@PathVariable(name = "id") int id) {
@@ -59,5 +64,38 @@ public class QualificacaoController {
         }
         return ResponseEntity.notFound().build();
 
+    }
+
+    @PostMapping
+    public ResponseEntity cadastrar(@RequestBody Qualificacao request) {
+        Qualificacao qualificacao = new Qualificacao();
+        qualificacao.setEmenta(request.getEmenta());
+        try {
+            repository.save(qualificacao);
+        } catch (IllegalArgumentException error) {
+            return ResponseEntity.badRequest().body("Dados Inválidos !");
+
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    /**
+     * @param id
+     * @param request
+     * @return
+     */
+    public ResponseEntity atualizar(@PathVariable int id, @RequestBody Qualificacao request) {
+        Qualificacao qualificacao = new Qualificacao();
+        qualificacao.setId(id);
+        qualificacao.setEmenta(request.getEmenta());
+
+        try {
+            repository.save(qualificacao);
+        } catch (IllegalArgumentException error) {
+            error.printStackTrace();
+            return ResponseEntity.badRequest().build();
+
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
